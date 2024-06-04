53d06d8260859b9e3da23fb191a42ecbb4a1e032
DiazLuisAlejandro <alejandrodiazluisv@gmail.com>
2024-06-04 18:40:13 +0100

    No se que estoy haciendo

---
+++ proyectoDB/src/main/java/es/ies/puerto/bae/proyectoDB/model/Pokemon.java
@@ -0,0 +1,10 @@
+package es.ies.puerto.bae.proyectoDB.model;
+
+import es.ies.puerto.bae.proyectoDB.enumerate.Type;
+
+public class Pokemon {
+    public int id;
+    public Type type1;
+    public Type type2;
+    public String name;
+}
